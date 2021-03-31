ec6c1c5faed97a2aa9be9ceb3629ab2d19493df8
ThomasAdons <61419261+ThomasAdons@users.noreply.github.com>
2021-03-31 17:12:59 +0200

    Update install.sh

--- Code/install.sh
+++ Code/install.sh
@@ -1,5 +1,20 @@
-#!/bin/sh
-sudo apt-get update && sudo apt-get upgrade -y 
-sudo rpi-update -y
-sudo apt-get autoremove -y
-sudo apt-get autoclean -y
+#!/bin/su root
+apt-get update -y 
+apt install python3-pip -y 
+apt install python3-cffi -y 
+pip3 install discord.py[voice]
+python3 -m pip install -U discord.py[voice]
+apt-get install build-essential python-dev -y
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+cd Adafruit_Python_DHT
+python setup.py install
+python3 setup.py install
+#apt-get autoremove -y
+#apt-get autoclean -y
+systemctl enable ssh
+systemctl start ssh
+raspi-config nonint do_camera 0
+mkdir ~/Desktop/IOT_Workshop
+cd ~/Desktop/IOT_Workshop
+touch Workshop.py
+reboot
