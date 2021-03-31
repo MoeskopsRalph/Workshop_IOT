e3d536ca671af6606653a098b7f9fccb612a3011
MoeskopsRalph <61419242+MoeskopsRalph@users.noreply.github.com>
2021-03-31 17:46:04 +0200

    Update bot.py
    Camera deel toegevoegd

--- Code/bot.py
+++ Code/bot.py
@@ -4,9 +4,10 @@ import time
 import random
 import datetime
 import RPi.GPIO as GPIO
-import time
 import asyncio
 import Adafruit_DHT
+from picamera import PiCamera, Color
+
 
 #DHT declareren
 dht_sensor = Adafruit_DHT.DHT11
@@ -20,9 +21,11 @@ GPIO.setup(pir_sensor, GPIO.IN, GPIO.PUD_DOWN) #voor ruis weg te werken
 current_state = 0
 
 #Toegang token voor discord server
-TOKEN = 'TOKENHERE'
+TOKEN = 'ODIzMjA4NDc4MjU5NTQ0MDg0.YFdeow.WD5nM_MJmuPlfRcsC5fuwnktwu8'
 description = '''raspberry in Python'''
 bot = commands.Bot(command_prefix='?', description=description)
+camera = PiCamera()
+camera.resolution=(2591,1944)
 
 #Stuurt "motion detected" bij beweging
 async def motionDetection():
@@ -37,6 +40,11 @@ async def motionDetection():
             if current_state == 1:
                 if not msg_sent:
                     await channel.send('Motion Detected')
+                    camera.start_recording('/home/pi/Desktop/IOT_Workshop/video.h264')
+                    sleep(5)
+                    camera.stop_recording()
+                    await channel.send(file=discord.File('/home/pi/Desktop/IOT_Workshop/video.h264'))
+
                     msg_sent = True
                 else:
                     msg_sent = False
@@ -45,6 +53,12 @@ async def motionDetection():
         except KeyboardInterrupt:
             GPIO.cleanup()
         await asyncio.sleep(1)
+
+@bot.command()
+async def foto(ctx):
+    """Bij dit commando wordt er een foto gemaakt en doorgestuurd."""
+    camera.capture('/home/pi/Desktop/IOT_Workshop/image.jpg')
+    await ctx.send(file=discord.File('/home/pi/Desktop/IOT_Workshop/image.jpg'))
 
 @bot.event
 async def on_ready():
